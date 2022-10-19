fe22ccc798f59997a69822a849e5c96219934bb3
Yuns <yuns.xie@qq.com>
2022-10-19 22:42:19 +0800

    chore: refactor: extract Invoke-RunAsAdministrator

--- powershell/Scripts/Update-WSL-Hosts.ps1
+++ powershell/Scripts/Update-WSL-Hosts.ps1
@@ -1,34 +1,9 @@
-#Read more: https://www.sharepointdiary.com/2015/01/run-powershell-script-as-administrator-automatically.html#ixzz7i4zQ9sKj
-Function Invoke-RunAsAdministrator {
-  #Get current user context
-  $CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  
-  #Check user is running the script is member of Administrator Group
-  if ($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
-    Write-host "Script is running with Administrator privileges!"
-  }
-  else {
-    #Create a new Elevated process to Start PowerShell
-    $ElevatedProcess = New-Object System.Diagnostics.ProcessStartInfo "PowerShell";
-
-    # Specify the current script path and name as a parameter
-    $ElevatedProcess.Arguments = "& '" + $script:MyInvocation.MyCommand.Path + "'"
-
-    #Set the Process to elevated
-    $ElevatedProcess.Verb = "runas"
-
-    #Start the new elevated process
-    [System.Diagnostics.Process]::Start($ElevatedProcess)
-
-    #Exit from the current, unelevated, process
-    Exit
-  }
-}
+. "$PSScriptRoot\Utils\Invoke-RunAsAdministrator.ps1"
 
 #Check Script is running with Elevated Privileges
 Invoke-RunAsAdministrator
 
-Import-Module $PSScriptRoot\..\Modules\Carbon.2.11.0\Carbon
+Import-Module "$PSScriptRoot\..\Modules\Carbon.2.11.0\Carbon"
 
 # 保证 WSL 名称正确，可通过 wsl --list 查看
 $DistroName = "Ubuntu-18.04"
@@ -38,7 +13,7 @@ $Hosts = @(
   "dev.app2.com"
 )
 
-$WslHostIP = ((PowerShell wsl -d $DistroName hostname -I) | Out-String).Trim()
+$WslHostIP = ((PowerShell "wsl -d $DistroName hostname -I") | Out-String).Trim()
 
 Foreach ($hostname in $hosts) {
   Write-Host $WslHostIP $hostname
@@ -47,4 +22,5 @@ Foreach ($hostname in $hosts) {
 
 # Remove-CHostsEntry -HostName 'myserver'
 
-pause
+Write-Host "Press any key to exit..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
